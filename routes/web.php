af0cc590d844ebd04d26c6c94e57cab75cd742c5
iliescumichele <iliescumichele@gmail.com>
2022-06-27 01:13:00 +0200

    Aggiunta del file css + stampa di un parametro dinamico

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+
+    $data = [
+        'parola' => 'LARAVEL'
+    ];
+
+    return view('home', $data);
 });
